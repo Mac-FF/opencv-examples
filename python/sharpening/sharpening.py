24e3be30969012fc62fa9d77a7c7a2a53828d701
Mac-FF <kmlajceps@gmail.com>
2017-05-19 12:31:09 +0200

    Add files via upload

--- python/sharpening/sharpening.py
+++ python/sharpening/sharpening.py
@@ -1,27 +1,46 @@
+#!/usr/bin/python
+"""**sharpening.py v1.0, Python v2.7.6 - 2.7.12, 2017, kmlajceps@gmail.com**
 
+- This module sharpens of images in dir 'sample' using::
+
+    OpenCV v2.4.9
+    NumPy v1.11.1"""
 import cv2
 import numpy as np
 import glob
-import argparse
+import os
+import time
 
 
-class Filter:
-    filter_size = 5
-    c = 3 + 2 * filter_size
-    kernel = np.zeros((c, c), np.float32)
-    kernel[c / 2, c / 2] = 2.0
-    box_filter = np.ones( (c, c), np.float32) / (c * c)
-    kernel = kernel - box_filter
+class Kernel:
+    def __init__(self):
+        self.size = 3
+        self.kernel = 0
+        self.calculate()
 
-def focus(image):
-
-    return cv2.filter2D(image,-1, kernel)
+    def calculate(self):
+        c = 3 + 2 * self.size
+        kernel = np.zeros((c, c), np.float32)
+        kernel[c / 2, c / 2] = 2.0
+        box_filter = np.ones((c, c), np.float32) / (c * c)
+        kernel = kernel - box_filter
+        self.kernel = kernel
+        return kernel
 
 
 def main():
-    print kernel
-    print np.shape(kernel)
+    print "Sharpen the images in dir 'sample'"
+    kernel = Kernel()
 
+    paths = glob.glob("sample/*.jpg")
+    for path in paths:
+        start = time.time()
+        name = os.path.basename(path)[:-4]
+        image = cv2.imread(path)
+        img_back = cv2.filter2D(image, -1, kernel.kernel)
+        cv2.imwrite(name + '_sharpened.jpg', img_back)
+        print path, "time: ", time.time() - start
+    print "End"
 
 if __name__ == '__main__':
     main()
